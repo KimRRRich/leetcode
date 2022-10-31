eb63bee624dd3f350e6cbbd410287d245584c685
zx <2452367222@qq.com>
2022-10-31 13:05:19 +0800

    Update 58.最后一个单词的长度.cpp

--- 58.最后一个单词的长度.cpp
+++ 58.最后一个单词的长度.cpp
@@ -8,6 +8,21 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        int length=s.length();
+        int maxLength=0;
+        int currentLength=0;
+        for(int i=0;i<length;i++){
+            if(s[i]!=' '){
+                if(i==length-1){
+                    currentLength+=1;
+                    if(0<currentLength) maxLength=currentLength;
+                }else currentLength+=1;
+            }else{
+                if(0<currentLength) maxLength=currentLength;
+                currentLength=0;
+            }
+        }
+        return maxLength;
 
     }
 };
