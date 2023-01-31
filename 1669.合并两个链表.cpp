b55ee065561242b6f8a6db7b63abe50abeacc1ab
zx <102405740+KimRRRich@users.noreply.github.com>
2023-01-31 16:21:02 +0800

    2023.01.30

--- 1669.合并两个链表.cpp
+++ 1669.合并两个链表.cpp
@@ -65,13 +65,13 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-// struct ListNode {
-//     int val;
-//     ListNode *next;
-//     ListNode() : val(0), next(nullptr) {}
-//     ListNode(int x) : val(x), next(nullptr) {}
-//     ListNode(int x, ListNode *next) : val(x), next(next) {}
-// };
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution {
 public:
     ListNode* mergeInBetween(ListNode* list1, int a, int b, ListNode* list2) {
